a0e2f5342f8db8e7dd13302fb5711a81e4e550df
Michael Davies <michael@the-davies.net>
2016-05-03 13:12:33 +0930

    Adding support for Mitaka

--- scripts/build-aio.sh
+++ scripts/build-aio.sh
@@ -21,16 +21,94 @@
 # 02111-1307, USA.
 #
 
+# v1.0 - Support Liberty and Mitaka, with Liberty from mrda's repo
+VERSION=1.0
+USAGE="\nBuild an AIO for an optionally specified release\n\n \
+-h|--help - display this help text\n \
+-v|--version - display version information about this program\n \
+-l|--list - list supported releases \n \
+release - the release to build (optional)"
+
+MYNAME="$(basename $0)"
+COPY="Copyright (C) 2016 Michael Davies <michael@the-davies.net>"
+SUPPORTED_RELS="\n  Liberty\n  Mitaka"
+
+# Make sure we're running as root
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root.  Exiting..." 2>&1
+  exit 1
+fi
+
+ARGS=("$@")
+case "$1" in
+    -h|--help)
+        echo "${MYNAME} [-h|-v|-l|--help|--version|--list] [release]"
+        echo "${USAGE}"
+        exit 1
+        ;;
+    -v|--version)
+        echo "${MYNAME}: ${VERSION}"
+        echo "${COPY}"
+        exit 1
+        ;;
+    -l|--list)
+        echo "${MYNAME}: The supported releases are: ${SUPPORTED_RELS}"
+        exit 1
+        ;;
+esac
+
+
+# Determine what release to build
+if [ $# -eq 0 ]; then
+  echo "--- No release requested, using the default"
+  RELEASE="Liberty"
+else
+  RELEASE=$1
+fi
+
+case "${RELEASE}" in
+    [Ll]iberty)
+        # Definitions for openstack-ansible Liberty version
+        echo "--- Building a Liberty AIO from mrda's repository"
+        REPO="https://github.com/mrda/openstack-ansible.git"
+        BRANCH="liberty-cba"
+        ;;
+    [Mm]itaka)
+        # Definitions for openstack-ansible Mitaka version
+        echo "--- Building a Mitaka AIO from OpenStack's official repository"
+        REPO="https://github.com/openstack/openstack-ansible.git"
+        BRANCH="mitaka"
+        ;;
+    *)
+        echo "Unsupported version specified"
+        exit 1
+        ;;
+esac
+
+# Required dependencies
+read -r -d '' DEPS << EOSTR
+git
+git-review
+ack-grep
+build-essential
+screen
+libssl-dev
+libffi-dev
+python-dev
+screen
+EOSTR
+
 # Install dependencies
-apt-get update && apt-get -y upgrade && apt-get -y install git git-review \
-  ack-grep build-essential screen libssl-dev libffi-dev python-dev screen
+apt-get update
+apt-get -y upgrade
+apt-get -y install ${DEPS}
 
 # Clone the repo
-git clone https://github.com/mrda/openstack-ansible.git /opt/openstack-ansible
+git clone ${REPO} /opt/openstack-ansible
 cd /opt/openstack-ansible
 
 # Go to the right branch
-git checkout liberty-cba
+git checkout ${BRANCH}
 
 # Ironic isn't turned on by default, so copy in the environment
 mkdir -p /etc/openstack_deploy/env.d/
@@ -48,6 +126,7 @@ openstack-ansible setup-everything.yml
 openstack-ansible os-ironic-install.yml
 
 # Test that it works
+echo "--- Quick test to see that ironic is working..."
 UTIL=$(lxc-ls | grep utility)
 lxc-attach -n $UTIL
 cd /root
